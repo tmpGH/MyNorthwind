9392000f2f37877942d9b36e44bbe53c1d56b900
TMP <tmp@onet.eu>
2023-01-19 19:37:40 +0100

    remove AppModule

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,10 +1,8 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
 
-//import { AppModule } from './app/app.module';
 import { APP_ROUTES } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
@@ -14,11 +12,4 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(APP_ROUTES)]
-  /* another way of use of standalone component */
-  //providers: [importProvidersFrom(AppModule)]
 });
-
-/* without standalone component */
-// platformBrowserDynamic()
-//   .bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
