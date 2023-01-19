732a481f720b5110005e8e0d81c8af2f8a300f25
TMP <tmp@onet.eu>
2023-01-19 14:00:58 +0100

    change project bootstrap to start from standalone component

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,5 +1,7 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +10,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(AppModule)]
+});
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
