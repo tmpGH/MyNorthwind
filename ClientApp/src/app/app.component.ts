da4521028621c77109093d7ed5c3ae275db3835d
TMP <tmp@onet.eu>
2023-01-22 22:50:45 +0100

    correct the behavior of the top menu

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event, RouterModule } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { APP_ROUTES } from './app-routing.module';
 
@@ -15,9 +15,25 @@ import { APP_ROUTES } from './app-routing.module';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyNorthwind';
-
   menuItems = APP_ROUTES;
-  activeMenuItemId = APP_ROUTES[0].title;
+  activeMenuItemId?: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.setActiveMenuItem();
+  }
+
+  setActiveMenuItem() {
+    this.router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd) {
+        let fragment = event.url.split('/')[1];
+        let activatedRoute = APP_ROUTES.find(r => r.path == fragment) ?? APP_ROUTES[0];
+        this.activeMenuItemId = activatedRoute.title?.toString();
+      }
+    });
+  }
+
 }
